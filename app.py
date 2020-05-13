4237509deab84534287d98fd6ccf17a5805c77e4
Graeme-B <64837200+Graeme-B@users.noreply.github.com>
2020-05-13 14:30:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from confluent_kafka import Producer
 
-p = Producer({'bootstrap.servers':'my-cluster-kafka-bootstrap:9092')
+p = Producer({'bootstrap.servers':'my-cluster-kafka-bootstrap:9092'})
 p.produce('my-topic',key='message',value='Hello from Python')
 p.flush(30)
